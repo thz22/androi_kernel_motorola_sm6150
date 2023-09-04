e0edf87fe32f04875ca2f703855fb1e3b33a38ac
Sultan Alsawaf <sultan@kerneltoast.com>
2023-09-04 21:44:17 +0000

    sched: Never migrate tasks upon execve()
    Explicitly return the previous CPU for SD_BALANCE_EXEC in order to never
    migrate tasks upon execve(). We don't know much about a task after it's
    called execve(), so there's not much task-specific information to go off of
    for load balancing.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [ EmanuelCN: Adapt to msm-4.19 with Sultan's help ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8538,6 +8538,9 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 			return cpu;
 		}
 
+	if (sd_flag & SD_BALANCE_EXEC)
+		return prev_cpu;
+
 		record_wakee(p);
 		want_energy = wake_energy(p, prev_cpu, sd_flag, wake_flags);
 		want_affine = !want_energy &&
